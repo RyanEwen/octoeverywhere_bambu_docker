de352e100961ef1830052ff23106807ad47ed882
Ryan Ewen <ryan.ewen@gmail.com>
2024-05-10 11:43:20 -0400

    Don't try to reinstall after container stopped and started again

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# generate json to pass to octoeverywhere installer
-PY_LAUNCH_JSON=$(jq -n \
-    --arg REPO_DIR "$REPO_DIR" \
-    --arg VENV_DIR "$VENV_DIR" \
-    --arg USER "$USER" \
-    --arg HOME "$HOME" \
-    --arg CMD_LINE_ARGS "-bambu" \
-    '{ OE_REPO_DIR: $REPO_DIR, OE_ENV: $VENV_DIR, USERNAME: $USER, USER_HOME: $HOME, CMD_LINE_ARGS: $CMD_LINE_ARGS }' )
+# perform inital setup if needed
+if [ ! -f /root/start.sh ]; then
+    # generate json to pass to octoeverywhere installer
+    PY_LAUNCH_JSON=$(jq -n \
+        --arg REPO_DIR "$REPO_DIR" \
+        --arg VENV_DIR "$VENV_DIR" \
+        --arg USER "$USER" \
+        --arg HOME "$HOME" \
+        --arg CMD_LINE_ARGS "-bambu" \
+        '{ OE_REPO_DIR: $REPO_DIR, OE_ENV: $VENV_DIR, USERNAME: $USER, USER_HOME: $HOME, CMD_LINE_ARGS: $CMD_LINE_ARGS }' )
 
-# start octoeverywhere installer
-# credit to https://github.com/ntebis/octoeverywhere_bambulab_docker for the way these get passed into the installer
-(echo ${ACCESS_CODE}; echo ${SERIAL_NUMBER}; echo "y" ; echo ${IP_ADDRESS}) | ${VENV_DIR}/bin/python3 -B -m py_installer ${PY_LAUNCH_JSON} || true
+    # start octoeverywhere installer
+    # credit to https://github.com/ntebis/octoeverywhere_bambulab_docker for the way these get passed into the installer
+    (echo ${ACCESS_CODE}; echo ${SERIAL_NUMBER}; echo "y" ; echo ${IP_ADDRESS}) | ${VENV_DIR}/bin/python3 -B -m py_installer ${PY_LAUNCH_JSON} || true
 
-# grab the start command from the service file and put it into its own script
-grep 'ExecStart=' /etc/systemd/system/octoeverywhere-bambu.service | sed -r 's/ExecStart=//' > /root/start.sh && chmod +x /root/start.sh
+    # grab the start command from the service file and put it into its own script
+    grep 'ExecStart=' /etc/systemd/system/octoeverywhere-bambu.service | sed -r 's/ExecStart=//' > /root/start.sh && chmod +x /root/start.sh
+fi
 
 # start the service
 /root/start.sh
